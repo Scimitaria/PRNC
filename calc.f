a91cb46f8cf2abba033ed481242b19b93e95ce0b
Morgan Powers <morgan.c.powers@gmail.com>
2025-06-16 11:40:02 -0500

    variables

--- calc.f
+++ calc.f
@@ -1,16 +1,27 @@
+\ get number
 : getNum
   PAD 10 ACCEPT  \ get command line input
   PAD SWAP       \ uses length of cli to parse input
   EVALUATE ;     \ interpret the string as a number
   \ TODO: use ?DIGIT to verify num?
 
+\ get operation
 : getOp
   PAD 1 ACCEPT
   PAD SWAP ;
 
+\ initialize variables
+0 VALUE num1
+0 VALUE num2
+0 VALUE op
+
+\ calculate
 : calc
   ." Enter two numbers" CR
-  getNum getNum + . CR
+  getNum TO num1
+  getNum TO num2
+  num1 num2 + . CR
   ." Enter the operation to perform" CR
-  getOp TYPE CR
+  getOp TO op
+  op TYPE CR
   bye ;
