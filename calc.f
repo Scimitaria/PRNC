b251c21f8cd21d18057106b203f413c7b6ec403d
Morgan Powers <morgan.c.powers@gmail.com>
2025-06-16 11:20:53 -0500

    op

--- calc.f
+++ calc.f
@@ -1,9 +1,16 @@
 : getNum
-  PAD 1 ACCEPT  \ get command line input
-  PAD SWAP      \ uses length of cli to parse input
-  EVALUATE ;    \ interpret the string as a number
+  PAD 10 ACCEPT  \ get command line input
+  PAD SWAP       \ uses length of cli to parse input
+  EVALUATE ;     \ interpret the string as a number
+  \ TODO: use ?DIGIT to verify num?
+
+: getOp
+  PAD 1 ACCEPT
+  PAD SWAP ;
 
 : calc
   ." Enter two numbers" CR
   getNum getNum + . CR
+  ." Enter the operation to perform" CR
+  getOp TYPE CR
   bye ;
